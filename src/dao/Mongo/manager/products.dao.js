d72ba361e6e490e03d693abf15cbd77f152ec536
Guillermo Lagras <guille_ivan@>
2024-03-16 15:23:03 -0300

    cambio de productsManager

--- src/dao/Mongo/manager/products.dao.js
+++ src/dao/Mongo/manager/products.dao.js
@@ -2,7 +2,7 @@
 import BasicManager from "./basic.dao.js";
 import { productsModel } from "../models/Product.model.js";
 
-export default class ProductManager extends BasicManager {
+export default class ProductsManager extends BasicManager {
   constructor() {
     super(productsModel, ''); 
 }
